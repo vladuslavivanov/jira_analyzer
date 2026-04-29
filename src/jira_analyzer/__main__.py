fe8a2f0176e645171a3f9cda8d1bc738d4b2d7fa
Ivan <i1turin@itnb-mbp.local>
2026-04-29 23:28:42 +0300

    make module runnable

--- src/jira_analyzer/__main__.py
+++ src/jira_analyzer/__main__.py
@@ -1,12 +1,31 @@
+import os
 import sys
-from jira_analyzer.cli import main
+from pathlib import Path
 
+if not __package__:
+    # Make CLI runnable from source tree with
+    #    python src/package
+    package_source_path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, package_source_path)
+    
 if __name__ == "__main__":
-    # This entry point allows running the package as:
-    # python -m jira_analyzer [args]
-    #
-    # To run the Streamlit UI, use:
-    # streamlit run src/jira_analyzer/ui.py
     
     # By default, we trigger the CLI logic
-    sys.exit(main())
+    if "--streamlit" in sys.argv:
+        from streamlit.web.cli import main as streamlit_main
+
+        from jira_analyzer import ui as web_ui_app
+
+        streamlit_args = [arg for arg in sys.argv[1:] if arg != "--streamlit"]
+        sys.argv = [
+            "streamlit",
+            "run",
+            str(Path(__file__).parent / f"{web_ui_app.__file__}"),
+        ] + streamlit_args
+
+        sys.exit(streamlit_main())
+    else:
+        from jira_analyzer.cli import main as cli_app
+
+        sys.exit(cli_app())
+    
